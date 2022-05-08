dba6e707d15b8921d8c8620823d4f7d8769a17eb
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-05-08 10:04:58 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    data.on('play', onPlay);
+    
+    // If later on you decide that you don’t need to listen for play anymore.
+    data.off('play', onPlay);
+    
+    // Alternatively, `off` can be called with just the event name to remove all
+    // listeners.
+    data.off('play');
+};
+
