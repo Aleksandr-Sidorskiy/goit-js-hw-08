a5fb0478e8ec0225c923ee05833f5f116bb9d582
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-05-09 15:45:26 +0300

    js2 and js3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,39 +1,12 @@
-import Vimeo from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
-// ______________________________________________
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe, options);
+const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-// ______________________________________________
-const options = {
-    duration: 61.857,
-    percent: 0.049,
-    seconds: 3.034,
-}
-iframe.addEventListener()
-function handTextAreaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
-  
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-
-const VIMEO_KEY = "videoplayer-current-time";
-populateTextarea();
-function populateTextarea() {
-    // const savedMessage = localStorage.getItem(JSON.stringify(formData));
-  const savedTime = localStorage.getItem(STORAGE_KEY);
-      console.log(savedMessage);
-  if (savedMessage) {
-      textareaEl.value = savedMessage;
-      
-  }
-    
-}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,6 @@
-import '../css/03-feedback.css';
-import '../css/common.css';
+
 import throttle from 'lodash.throttle';//
 
-
-// const textareaEl = document.querySelector(".feedback-form textarea");
-// const emailEl = document.querySelector(".feedback-form input");
-
-// formEl.addEventListener('submit', handFormSabmit);
-// textareaEl.addEventListener('input', throttle(handTextAreaInput, 500));
-// emailEl.addEventListener('input', throttle(handTextAreaInput, 500));
 const formEl = document.querySelector(".feedback-form");//
 
 formEl.addEventListener('input', throttle(handInputType, 500));
@@ -16,63 +8,30 @@ formEl.addEventListener('submit', handFormSubmit);
 
 const STORAGE_KEY = "feedback-form-state";
 
-// let formData =  JSON.parse(localStorage.getItem(STORAGE_KEY)) || {} ;//
-
-function handInputType(evt) {
-    const inputName = evt.target.name;
-    const inputValue = evt.target.value;
-    formData[inputName] = inputValue;
-    localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
-    console.log(formData)
+const formData =  {} ;
+// сохраняет локально ключ и значение ключа в обьект
+function handInputType(e) {
+    formData[e.target.name] = e.target.value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+        console.log(formData);
 }
-    
-    // formEl.addEventListener('input', e => {
-    //     // console.log(e.target.name);
-    //     // console.log(e.target.value);
-    //     // JSON.stringify(formData[e.target.name] = e.target.value);
-    //     formData[e.target.name] = e.target.value;
-    //     console.log(formData);
-    // });
-
-
-populateForm();
-function populateForm() {
-    // const savedMessage = localStorage.getItem(JSON.stringify(formData));
-    const savedForm = localStorage.getItem(STORAGE_KEY);
-    const parsedForm = JSON.parse(savedForm);
-      console.log(savedForm);
-  if (parsedForm) {
-     formEl.elements.message.value = parsedForm.message || '';
-    formEl.elements.email.value = parsedForm.email || '';
-  }
-}
-
+// выводит в консоль значение email and message
 function handFormSubmit(evt) {
     evt.preventDefault();
     
-    const {
-        elements: { email, message }
-    } = evt.currentTarget;
-    
-    if (email.value === "" || message.value === "") {
-        return console.log("Please fill in all the fields!");
-    }
-    
-    console.log(`email: ${email.value},textarea: ${message.value}`);
-    localStorage.removeItem(STORAGE_KEY);
-    evt.currentTarget.reset();
+   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-// function handTextAreaInput(evt) {
-//     const message = evt.target.value;
-//     localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// let formData =  JSON.parse(savedMessage)||{};
-// let formData = {};
-
-// const savedMessage = localStorage.getItem(STORAGE_KEY);
-// console.log(savedMessage);
-
-
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
 
